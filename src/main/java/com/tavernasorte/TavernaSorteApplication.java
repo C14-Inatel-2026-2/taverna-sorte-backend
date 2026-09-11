8395cf83974a2b87cf90e909c81108b79a148de2
LuizGuimaraesz <luizgustavogs123123@gmail.com>
2026-09-11 18:08:51 -0300

    chore: configuração do spring boot

---
+++ src/main/java/com/tavernasorte/TavernaSorteApplication.java
@@ -0,0 +1,12 @@
+package com.tavernasorte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TavernaSorteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TavernaSorteApplication.class, args);
+    }
+}
