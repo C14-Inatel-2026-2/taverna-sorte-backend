5d1ba646f8d9f5e7e7a8ac0d53395c2175b80d48
DavidFBar <davidfilhobarbon@gmail.com>
2026-09-11 17:39:01 -0300

    Criando paramêtros iniciais da classe Item

--- src/main/java/model/Item.java
+++ src/main/java/model/Item.java
@@ -1,3 +1,21 @@
 package model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // gerando o ID do item automaticamente
+    private Long id; // Long aceita valores nulos antes do ID ser gerado pelo banco
+
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Item(){
+
+    }
 }
