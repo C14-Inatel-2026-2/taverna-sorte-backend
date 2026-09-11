7d40995aba3cd8ed34378e0f6f23b32d93cb20f5
DavidFBar <davidfilhobarbon@gmail.com>
2026-09-11 18:10:45 -0300

    Realizando mais testes da classe Item

--- src/main/java/model/Item.java
+++ src/main/java/model/Item.java
@@ -13,9 +13,28 @@ public class Item {
 
     private String nome;
     private double preco;
-    private int quantidade;
 
-    public Item(){
+    public Item() {
 
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
--- src/test/java/model/ItemTeste.java
+++ src/test/java/model/ItemTeste.java
@@ -1,12 +1,31 @@
 package model;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ItemTeste {
     @Test
     void itemCriado(){
         Item item = new Item();
         assertNotNull(item);
+    }
+    @Test
+    void setNomeTeste(){
+        Item item = new Item();
+        item.setNome("Poção");
+        assertEquals("Poção", item.getNome());
+    }
+
+    @Test
+    void setPrecoTeste(){
+        Item item = new Item();
+        item.setPreco(5);
+        assertEquals(5, item.getPreco());
+    }
+
+    @Test
+    void getIdTeste(){
+        Item item = new Item();
+        assertNull(item.getId());
     }
 }
